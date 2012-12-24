73d0b3ff0e360ee0956b9d1ebb739a7737445354
Dan Leehr <dan@hammockdistrict.com>
2012-12-24 12:43:55 -0500

    Add comment on block typedef to indicate what the array objects are

--- DLSFTPClient/Classes/DLSFTPConnection.h
+++ DLSFTPClient/Classes/DLSFTPConnection.h
@@ -46,7 +46,7 @@ typedef enum {
 
 typedef void(^DLSFTPClientSuccessBlock)(void);
 typedef void(^DLSFTPClientFailureBlock)(NSError *error);
-typedef void(^DLSFTPClientArraySuccessBlock)(NSArray *array);
+typedef void(^DLSFTPClientArraySuccessBlock)(NSArray *array); // Array of DLSFTPFile objects
 typedef BOOL(^DLSFTPClientProgressBlock) (unsigned long long bytesReceived, unsigned long long bytesTotal); // return NO for cancel
 typedef void(^DLSFTPClientFileTransferSuccessBlock)(DLSFTPFile *file, NSDate *startTime, NSDate *finishTime);
 typedef void(^DLSFTPClientFileMetadataSuccessBlock)(DLSFTPFile *fileOrDirectory);
