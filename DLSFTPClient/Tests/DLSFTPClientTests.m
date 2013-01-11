eb860f2a51a09da0dc278cb0d940b7006965094c
Dan Leehr <dan@hammockdistrict.com>
2013-01-10 22:30:15 -0500

    Implemented testUpload

--- DLSFTPClient/Tests/DLSFTPClientTests.m
+++ DLSFTPClient/Tests/DLSFTPClientTests.m
@@ -15,6 +15,7 @@
 
 @property (strong, nonatomic) NSDictionary *connectionInfo;
 @property (strong, nonatomic) DLSFTPConnection *connection;
+@property (strong, nonatomic) NSString *testFilePath;
 
 @end
 
@@ -33,6 +34,10 @@
     self.connection = connection;
     STAssertNotNil(self.connection, @"Connection is nil");
 
+    NSString *testFilePath = [[NSBundle bundleWithIdentifier:@"com.hammockdistrict.DLSFTPClientTests"] pathForResource:@"testfile"
+                                                                                                                ofType:@"jpg"];
+    self.testFilePath = testFilePath;
+    STAssertNotNil(self.testFilePath, @"Test file path is nil");
 }
 
 - (void)tearDown
@@ -153,23 +158,77 @@
     STAssertNil(localError, localError.localizedDescription);
 }
 
+- (void)testUpload {
+    [self testConnect];
+    STAssertTrue([self.connection isConnected], @"Not connected");
+    __block NSError *localError = nil;
+    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+
+    NSString *basePath = self.connectionInfo[@"basePath"];
+    NSString *fileName = [self.testFilePath lastPathComponent];
+    NSString *destPath = [basePath stringByAppendingPathComponent:fileName];
+    NSDictionary *localFileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:self.testFilePath
+                                                                                         error:&localError];
+    STAssertNil(localError, localError.localizedDescription);
+
+    [self.connection uploadFileToRemotePath:destPath
+                              fromLocalPath:self.testFilePath
+                              progressBlock:^BOOL(unsigned long long bytesReceived, unsigned long long bytesTotal) {
+                                  return YES;
+                              } successBlock:^(DLSFTPFile *file, NSDate *startTime, NSDate *finishTime) {
+                                  dispatch_semaphore_signal(semaphore);
+                              } failureBlock:^(NSError *error) {
+                                  localError = error;
+                                  dispatch_semaphore_signal(semaphore);
+                              }];
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    STAssertNil(localError, localError.localizedDescription);
+
+    // make sure the file listing reflects the same size
+    semaphore = dispatch_semaphore_create(0);
+    [self.connection listFilesInDirectory:basePath
+                             successBlock:^(NSArray *array) {
+                                 __block BOOL foundFile = NO;
+                                 [array enumerateObjectsUsingBlock:^(DLSFTPFile *file, NSUInteger idx, BOOL *stop) {
+                                     if ([file.filename isEqualToString:fileName]) {
+                                         *stop = foundFile = YES;
+                                         STAssertEquals(  file.attributes.fileSize
+                                                        , localFileAttributes.fileSize
+                                                        , @"Uploaded file size does not match local file size");
+                                     }
+                                 }];
+                                 STAssertTrue(foundFile, @"Uploaded file not found in listing");
+                                 dispatch_semaphore_signal(semaphore);
+                             } failureBlock:^(NSError *error) {
+                                 localError = error;
+                                 dispatch_semaphore_signal(semaphore);
+                             }];
+    dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+    STAssertNil(localError, localError.localizedDescription);
+}
 
 /*
  
 - (void)testRename {
- 
+
 }
+
+ 
+- (void)testCancelUpload {
+
+}
+
+ - (void)testCancelDownload {
+
+ }
+
 
 
 - (void)testDownload {
     STFail(@"Not yet implemented");    
 }
 
-- (void)testUpload {
-    STFail(@"Not yet implemented");
-}
-
-- (void)testUploadAndDownload {
+ - (void)testUploadAndDownload {
     STFail(@"Not yet implemented");
 }
 */
