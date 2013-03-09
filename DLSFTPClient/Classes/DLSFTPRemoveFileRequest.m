61d143192c7f979c0b2a833b238fcd4629e2ee6d
Dan Leehr <dan@hammockdistrict.com>
2013-03-09 10:41:44 -0500

    Implement DLSFTPRemoveFileRequest

---
+++ DLSFTPClient/Classes/DLSFTPRemoveFileRequest.m
@@ -0,0 +1,101 @@
+//
+//  DLSFTPRemoveFileRequest.m
+//  DLSFTPClient
+//
+//  Created by Dan Leehr on 3/9/13.
+//  Copyright (c) 2013 Dan Leehr. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//  Redistributions of source code must retain the above copyright notice,
+//  this list of conditions and the following disclaimer.
+//
+//  Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "DLSFTPRemoveFileRequest.h"
+#import "DLSFTPConnection.h"
+
+@interface DLSFTPRemoveFileRequest ()
+
+@property (nonatomic, copy) NSString *filePath;
+
+@end
+
+@implementation DLSFTPRemoveFileRequest
+
+- (id)initWithFilePath:(NSString *)filePath
+          successBlock:(DLSFTPClientSuccessBlock)successBlock
+          failureBlock:(DLSFTPClientFailureBlock)failureBlock {
+    self = [super init];
+    if (self) {
+        self.filePath = filePath;
+        self.successBlock = successBlock;
+        self.failureBlock = failureBlock;
+    }
+    return self;
+}
+
+- (void)start {
+    if (   [self pathIsValid:self.filePath] == NO
+        || [self ready] == NO
+        || [self checkSftp] == NO) {
+        [self.connection requestDidFail:self withError:self.error];
+        return;
+    }
+    LIBSSH2_SESSION *session = [self.connection session];
+    LIBSSH2_SFTP *sftp = [self.connection sftp];
+    int socketFD = [self.connection socket];
+    // sftp is now valid
+
+    int result;
+    while(  ((result = (libssh2_sftp_unlink(sftp, [self.filePath UTF8String]))) == LIBSSH2SFTP_EAGAIN)
+          && self.isCancelled == NO) {
+        waitsocket(socketFD, session);
+    }
+
+    if ([self ready] == NO) {
+        [self.connection requestDidFail:self withError:self.error];
+        return;
+    }
+
+    if (result) {
+        // unable to remove
+        NSString *errorDescription = [NSString stringWithFormat:@"Unable to remove file: SFTP Status Code %d", result];
+        self.error = [self errorWithCode:eSFTPClientErrorUnableToRemove
+                        errorDescription:errorDescription
+                         underlyingError:@(result)];
+        [self.connection requestDidFail:self withError:self.error];
+        return;
+    }
+    [self.connection requestDidComplete:self];
+}
+
+- (void)succeed {
+    DLSFTPClientSuccessBlock successBlock = self.successBlock;
+    if (successBlock) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            successBlock();
+        });
+    }
+    self.successBlock = nil;
+    self.failureBlock = nil;
+}
+
+@end
